9343e65c51a2d02740a630a4d6a479f72e5f822d
Documenter.jl <documenter@juliadocs.github.io>
2025-09-02 08:58:07 +0000

    build based on 19be29e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PrGK","page":"Home","title":"PrGK","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PrGK.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
