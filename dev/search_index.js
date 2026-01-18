cc15628d369c341ef1c90d37ee405116876c7749
Documenter.jl <documenter@juliadocs.github.io>
2026-01-18 13:04:02 +0000

    build based on 7c86ca1

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#PrGK","page":"Home","title":"PrGK","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PrGK.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#PrGK","page":"Home","title":"PrGK","text":"Documentation for PrGK.\n\n","category":"section"}]
 }
